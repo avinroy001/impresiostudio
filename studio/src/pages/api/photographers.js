c5e0577fa8384720d58829a8c45a4b7150d19f07
Avinash <avin.roy001@gmail.com>
2025-07-12 11:28:28 +0530

    handler update

--- studio/src/pages/api/photographers.js
+++ studio/src/pages/api/photographers.js
@@ -1,10 +1,15 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'studio', 'db.json');
-  const jsonData = fs.readFileSync(filePath);
-  const data = JSON.parse(jsonData);
+  try {
+    const filePath = path.join(process.cwd(), "src", "db.json"); 
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
 
-  res.status(200).json(data.photographers);
+    res.status(200).json(data.photographers); 
+  } catch (error) {
+    console.error("Failed to read db.json", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
