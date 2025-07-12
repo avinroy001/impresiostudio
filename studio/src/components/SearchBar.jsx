149fb73c7bb55162bec7b49a189d50c6c7227d0d
Avinash <avin.roy001@gmail.com>
2025-07-12 13:20:32 +0530

    debounce applied

--- studio/src/components/SearchBar.jsx
+++ studio/src/components/SearchBar.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setFilters, applyFilters } from "../redux/photographersSlice";
 
 const SearchBar = () => {
+  const [query, setQuery] = useState("");
   const dispatch = useDispatch();
 
-  const handleChange = (e) => {
-    dispatch(setFilters({ search: e.target.value }));
-    dispatch(applyFilters());
-  };
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      dispatch(setFilters({ search: query }));
+      dispatch(applyFilters());
+    }, 1000); 
+
+    return () => clearTimeout(delayDebounce);
+  }, [query, dispatch]);
 
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Search by name, location or tag" onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="Search by name, location, or tags"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
     </div>
   );
 };
+
 export default SearchBar;
